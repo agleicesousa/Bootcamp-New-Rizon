9a2feeb172eab3fac380dc61c7ef6fd0b7f5fedd
agleicesousa <agleicesousa98@gmail.com>
2024-09-30 16:51:31 -0300

    feat: arquivo incial exercicio 2

--- src/exercicio2/script.ts
+++ src/exercicio2/script.ts
@@ -1,1 +1,14 @@
-console.log("Olá mundo!");
+// Definindo o tipo para os itens da lista
+interface Item {
+    id: number;
+    name: string;
+    bio: string;
+};
+
+// Lista de biografias
+let lista: Item[] = [
+    { id: 1, name: "Ada Lovelace", bio: "Matemática e escritora inglesa, pioneira em algoritmos." },
+    { id: 2, name: "Alan Turing", bio: "Pai da ciência da computação e da inteligência artificial." },
+    { id: 3, name: "Nikola Tesla", bio: "Inventor e engenheiro conhecido por suas contribuições à eletricidade." },
+    { id: 4, name: "Nicolau Copérnico", bio: "Astrônomo que desenvolveu a teoria heliocêntrica." }
+];
