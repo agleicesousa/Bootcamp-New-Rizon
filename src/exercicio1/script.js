2307c276853a43c556596304fcc07e8a0c73df58
agleicesousa <agleicesousa98@gmail.com>
2024-09-30 16:14:57 -0300

    feat: função para contar vogais

--- src/exercicio1/script.js
+++ src/exercicio1/script.js
@@ -1,1 +1,7 @@
-console.log("Olá mundo!");
+// Função que retorne a quantidade de vogais da palavra passada
+function contarVogais(palavra) {
+    return Array.from(palavra).filter(function (letra) { return "aeiouAEIOU".includes(letra); }).length;
+}
+;
+// teste
+console.log(contarVogais("JavaScript"));
--- src/exercicio1/script.ts
+++ src/exercicio1/script.ts
@@ -1,1 +1,7 @@
-console.log("Olá mundo!");
+// Função que retorne a quantidade de vogais da palavra passada
+function contarVogais(palavra: string): number {
+    return Array.from(palavra).filter(letra => "aeiouAEIOU".includes(letra)).length;
+};
+
+// teste
+console.log(contarVogais("JavaScript"));
