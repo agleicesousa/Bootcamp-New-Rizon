8ca2c6bbf09bdc7070629a29c77ca237a38a3005
agleicesousa <agleicesousa98@gmail.com>
2024-09-30 16:20:37 -0300

    feat: por parâmetro

--- src/exercicio1/script.ts
+++ src/exercicio1/script.ts
@@ -2,6 +2,12 @@
 function contarVogais(palavra: string): number {
     return Array.from(palavra).filter(letra => "aeiouAEIOU".includes(letra)).length;
 };
-
 // teste
 console.log(contarVogais("JavaScript"));
+
+
+// Exemplo de uso com uma palavra recebida via parâmetros
+const palavra = "exemplo";
+const quantidadeVogais = contarVogais(palavra);
+//teste
+console.log(`A palavra "${palavra}" contém ${quantidadeVogais} vogais.`);
